fe15264f3a965712ac42f2a6a23388aa7cdf4d23
Aditya <adityamsahani9819@gmail.com>
2026-04-02 14:34:45 +0530

    fixed path error

--- server/index.js
+++ server/index.js
@@ -465,11 +465,13 @@ const staticDir = join(__dirname, "..", "dist", "public");
 
 if (isProduction && existsSync(staticDir)) {
   app.use(express.static(staticDir));
-  app.get("*", (req, res) => {
+
+  app.get('/:path(*)', (req, res) => {
     if (!req.path.startsWith("/api/") && !req.path.startsWith("/socket.io/")) {
       res.sendFile(join(staticDir, "index.html"));
     }
   });
+
   console.log(`✓ Serving static files from ${staticDir}`);
 }
 
